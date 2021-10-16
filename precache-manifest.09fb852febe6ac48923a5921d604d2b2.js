00b764963c7ccae87694189f501ea89507f9ac4e
Alex Varga <alexvarga09072001@gmail.com>
2021-10-16 13:56:22 +0300

    change gitignore file

---
+++ precache-manifest.09fb852febe6ac48923a5921d604d2b2.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51716a35a699229dfd3b",
+    "url": "/test_for_one-vision/css/app.5771b29e.css"
+  },
+  {
+    "revision": "7bde3d992a6e65a10271",
+    "url": "/test_for_one-vision/css/chunk-vendors.5c62e0f1.css"
+  },
+  {
+    "revision": "e811a90a0ef78bcc47b3e12f1dea30dc",
+    "url": "/test_for_one-vision/db.json"
+  },
+  {
+    "revision": "0116a4ae672182a834dc7ed3457d0c0a",
+    "url": "/test_for_one-vision/fonts/HelveticaNeue-CondensedRegular.0116a4ae.ttf"
+  },
+  {
+    "revision": "2118c2fbf7f1672ff35bc65aa2e4fcc0",
+    "url": "/test_for_one-vision/fonts/HelveticaNeue-CondensedRegular.2118c2fb.eot"
+  },
+  {
+    "revision": "e441825e90f592f29460e0e939e343fc",
+    "url": "/test_for_one-vision/fonts/HelveticaNeue-CondensedRegular.e441825e.woff"
+  },
+  {
+    "revision": "2a61b00fcc9b1afbf99be145777b1b72",
+    "url": "/test_for_one-vision/fonts/MaterialIcons-Regular.2a61b00f.eot"
+  },
+  {
+    "revision": "51cf1d641ae503ece727e7d84b182524",
+    "url": "/test_for_one-vision/fonts/MaterialIcons-Regular.51cf1d64.ttf"
+  },
+  {
+    "revision": "703cf8f274fbb265d49c6262825780e1",
+    "url": "/test_for_one-vision/fonts/MaterialIcons-Regular.703cf8f2.woff2"
+  },
+  {
+    "revision": "84a37de85c17f186652a179b1145392f",
+    "url": "/test_for_one-vision/fonts/MaterialIcons-Regular.84a37de8.woff"
+  },
+  {
+    "revision": "cf1110b6cc309a367c54658981408dcf",
+    "url": "/test_for_one-vision/img/HelveticaNeue-CondensedRegular.cf1110b6.svg"
+  },
+  {
+    "revision": "9f0176c5a0ef25961a3506040f517c9f",
+    "url": "/test_for_one-vision/img/Logo.9f0176c5.svg"
+  },
+  {
+    "revision": "c95fa3363e185e594c00950f23804fbd",
+    "url": "/test_for_one-vision/img/button-icon.c95fa336.svg"
+  },
+  {
+    "revision": "08c63fa535917e8a510229def5a2d374",
+    "url": "/test_for_one-vision/img/green.08c63fa5.svg"
+  },
+  {
+    "revision": "05ed1e8b3939d97a80ae55e4a9606958",
+    "url": "/test_for_one-vision/img/red.05ed1e8b.svg"
+  },
+  {
+    "revision": "433422edd3e4a568cd7261bee51bf2fd",
+    "url": "/test_for_one-vision/index.html"
+  },
+  {
+    "revision": "51716a35a699229dfd3b",
+    "url": "/test_for_one-vision/js/app.f37115f1.js"
+  },
+  {
+    "revision": "7bde3d992a6e65a10271",
+    "url": "/test_for_one-vision/js/chunk-vendors.9494f4be.js"
+  },
+  {
+    "revision": "5aca42f91ec184fe2b292e41a647df09",
+    "url": "/test_for_one-vision/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/test_for_one-vision/robots.txt"
+  }
+]);
